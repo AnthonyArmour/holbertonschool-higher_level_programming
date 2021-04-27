7d77f4626b95e34c369f68b967e3aa48a8ba9e89
AnthonyArmour <anthonydarmour@gmail.com>
2021-04-27 21:31:01 +0000

    fixed task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(101):
+        if x == 0:
+            continue
         if x % 3 == 0 and x % 5 == 0:
             print("{}".format("FizzBuzz"), end=" ")
         elif x % 5 == 0:
