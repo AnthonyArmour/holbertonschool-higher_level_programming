30cc79f2432e554482f4642a062dc0c6586dd36e
AnthonyArmour <anthonydarmour@gmail.com>
2021-04-28 19:25:07 +0000

    debug

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    num = 0
     if not my_list:
         return None
     else:
+        num = 0
         for x in my_list:
             if x > num:
                 num = x
