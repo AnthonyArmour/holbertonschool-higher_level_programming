7d52234d5e27a8d2a35f45c58662d93e7d7deda2
AnthonyArmour <anthonydarmour@gmail.com>
2021-04-28 17:20:57 +0000

    debug task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for n in my_list:
-        print("{}".format(n))
+    if __name__ == "__main__":
+        for n in my_list:
+            print("{}".format(n))
