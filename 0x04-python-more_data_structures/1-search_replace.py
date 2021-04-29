ae2020196cbc24b8ec68d84f72d3a0a0de6dd744
AnthonyArmour <anthonydarmour@gmail.com>
2021-04-29 20:37:39 +0000

    added task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
     n = 0
     nlst = list()
     while n < len(my_list):
-        nlst.append(my_list[n])
         if my_list[n] == search:
-            nlst.append(replace)
+            nlst.append(int(replace))
+        nlst.append(my_list[n])
         n = n + 1
     return nlst
