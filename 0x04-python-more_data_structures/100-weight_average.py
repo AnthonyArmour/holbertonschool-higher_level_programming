a7711d63a84db8aeed31e3068082c043351cb605
AnthonyArmour <anthonydarmour@gmail.com>
2021-04-30 01:16:24 +0000

    added task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return 0
     tot = 0
     div = 0
     for i in my_list:
         tot = tot + (int(i[0]) * int(i[1]))
         div = div + int(i[1])
+    if tot == 0:
+        return 0
     return float(tot / div)
