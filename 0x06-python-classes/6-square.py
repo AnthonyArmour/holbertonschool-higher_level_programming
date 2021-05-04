8c56a82c167325339bb3925a464fd3dddd44f272
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-04 17:13:58 +0000

    docs task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Private variable size, @property returns size and @size.setter sets size"""
+
+
+class Square:
+    """square defined by its elements"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """getter func"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter func"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    @property
+    def position(self):
+        """getter func"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter func"""
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """returns square of argument"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints square of #'s size of size squared"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for t in range(self.__position[0]):
+                    print("{}".format(" "), end="")
+                for n in range(self.__size):
+                    print("{}".format("#"), end="")
+                print()
