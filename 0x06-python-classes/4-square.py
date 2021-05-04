084cb9552a70fa7275cbdf86a5a44c1d27316000
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-04 17:05:38 +0000

    docs task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Private variable size, @property returns size and @size.setter sets size"""
+
+
+class Square:
+"""square is defined by its parameters"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """size is a getter func"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """is a setter func"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def area(self):
+        """returns square of argument"""
+        return self.__size ** 2
