5c15c5f9ce6e84d3f5d4d61f58bbe8e9f088f3b6
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-05 04:02:45 +0000

    added task 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Private variable size, @property returns size and @size.setter sets size"""
+
+
+class Square:
+    """square is defined by its elements"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """size is a getter func"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """is a setter func"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def area(self):
+        """returns square of argument"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 == other.__size ** 2)
+
+    def __ne__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 != other.__size ** 2)
+
+    def __lt__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 < other.__size ** 2)
+
+    def __le__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 <= other.__size ** 2)
+
+    def __gt__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 > other.__size ** 2)
+
+    def __ge__(self, other):
+        """Square comparators"""
+        return (self.__size ** 2 >= other.__size ** 2)
