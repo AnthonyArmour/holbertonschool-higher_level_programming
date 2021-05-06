0d6736e54df1c14a275f3592c849503078baf459
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-06 19:53:29 +0000

    debugg task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""say my name module"""
+
+
 def say_my_name(first_name, last_name=""):
+    """this module says name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    if last_name is not None:
+    if last_name is None and first_name is None:
+        print()
+    elif last_name is not None:
         print("My name is {} {}".format(first_name, last_name))
     else:
         print("My name is {}".format(first_name))
