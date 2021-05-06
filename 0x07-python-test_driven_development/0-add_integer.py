96524a51f3f1bce78043b9f6fc162fd76f81864c
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-06 18:03:00 +0000

    debuugg task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,6 +8,10 @@ def add_integer(a, b=98):
     """
     returns a + b
     """
+    if a is None:
+        raise TypeError("a must be an integer")
+    if b is None:
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
