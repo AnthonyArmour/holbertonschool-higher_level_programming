6f64617039827cc7d67f15fc40cbcec9cd6a19aa
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-06 17:49:23 +0000

    comment fix task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+add_integer returns sum
+"""
+
+
 def add_integer(a, b=98):
+    """
+    returns a + b
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     elif type(b) is not int and type(b) is not float:
