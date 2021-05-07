35a9e8ce104393b9d5deefb20d63ee443d3ba2d4
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-07 16:12:59 +0000

    debuggg task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""replaces ?.: with a \n"""
+
+
 def text_indentation(text):
+    """relaces .?: with \n"""
     sen = ":?."
+    x = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
     else:
-        for x in range(int(len(text))):
-            if text[x - 1] in sen:
+        while x < int(len(text)):
+            print("{}".format(text[x]), end="")
+            if text[x] in sen:
                 print("{}".format("\n"))
-            else:
-                print("{}".format(text[x]), end="")
+                if text[x + 1] == " " or text[x + 1] == "\t":
+                    while text[x + 1] == " ":
+                        x += 1
+            x += 1
