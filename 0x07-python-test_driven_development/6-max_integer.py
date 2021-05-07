e22fa7b73a9ebc8daf075a5bd63ebb6f8d2a5964
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-07 16:31:54 +0000

    readd task 6

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -7,8 +7,6 @@ def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
-    if len(list) == 0:
-        raise TypeError("must be a list of integers")
     result = list[0]
     i = 1
     while i < len(list):
