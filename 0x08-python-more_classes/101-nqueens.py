20d7f093a1735c6496a30733a11c87b2829a622e
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-08 23:50:40 +0000

    debug task 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,40 +11,25 @@ def solve_n_queens(queens):
         raise TypeError("must be an int")
     board = list()
     lst = list()
+    y = 0
     for i in range(queens):
         for n in range(queens):
             lst.append(0)
         board.append(list(lst))
         lst.clear()
-    solutionQ(board)
+    inception(board, y)
 
 
-def solutionQ(board):
-    """finds each solution and prints one by one"""
-    mlen = len(board)
-    x = 0
-    y = 1
-    res = False
-    while x < len(board):
-        board[x][0] = 1
-        res, board = inception(board, y, res)
-        if res is not False:
-            print_res(board)
-        board = reset_board(board)
-        x += 1
-        res = False
-
-
-def inception(board, y, res):
+def inception(board, y, res=False):
     """checks for an (all safe) combo"""
     xx = 0
-    res = False
     if y == len(board):
-        return True, board
+        print_res(board)
+        return
     while xx < len(board):
         if check_safe_spot(board, xx, y):
             board[xx][y] = 1
-            res, board = inception(board, y + 1, res)
+            res = inception(board, y + 1, res)
             if res is True:
                 return True, board
             board[xx][y] = 0
@@ -76,7 +61,7 @@ def check_safe_spot(board, xx, y):
         xn -= 1
     xn = xx + 1
     yn = y - 1
-    while xn < len(board) and y >= 0:
+    while xn < len(board) and yn >= 0:
         if board[xn][yn] == 1:
             res = False
         xn += 1
@@ -102,18 +87,6 @@ def print_res(board):
         x += 1
     print("{}".format(matrix))
 
-
-def reset_board(board):
-    """resets the board"""
-    x = 0
-    y = 0
-    while x < len(board):
-        while y < len(board):
-            board[x][y] = 0
-            y += 1
-        y = 0
-        x += 1
-    return board
 
 if len(sys.argv) != 2:
     print("{}".format("Usage: nqueens N"))
