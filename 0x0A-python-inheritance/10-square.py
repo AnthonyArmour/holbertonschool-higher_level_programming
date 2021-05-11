455c3c1b2677b7c26cfba55a6f30cd00f0b36736
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-11 21:53:08 +0000

    debug task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,13 +4,16 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
     """square class inherited from rectangle class"""
     def __init__(self, size):
         """init method"""
-        super(Square, self).__init__(size, size)
-        if self.integer_validator("size", size):
+        if super().integer_validator("size", size):
+            super(Square, self).__init__(size, size)
             self.__size = size
-            self.__height = size
             self.__width = size
+            self.__height = size
+
+    def area(self):
+        """area of square"""
+        return (self.__size * self.__size)
