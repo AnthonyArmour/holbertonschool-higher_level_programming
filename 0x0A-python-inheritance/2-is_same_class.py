13998daffb1b23507adbc7967ac126303621d09f
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-11 18:14:01 +0000

    added task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""returns true if same class"""
+
+
+def is_same_class(obj, a_class):
+    if a_class.__name__ == "object":
+        return False
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
