f4501ca01147cd4c0929fe08c0e92beae7180138
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-11 19:11:58 +0000

    added task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class with empty area method"""
+
+
+class BaseGeometry():
+    """class with one empty method"""
+    def area(self):
+        """empty method"""
+        raise Exception("area() is not implemented")
