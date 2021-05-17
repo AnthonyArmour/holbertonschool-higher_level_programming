4625bc5b8f314d419d983202e0040dff86a345a4
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-17 00:51:33 +0000

    added tsk 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """this is the base class"""
 
+import json
+
 
 class Base():
     """Base class"""
@@ -13,3 +15,25 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns json string representation"""
+        if len(list_dictionaries) == 0 or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes json string to a file"""
+        lst = str(cls).split("'")
+        lst2 = lst[1].split(".")
+        json_file = lst2[-1][:] + ".json"
+        objs = list()
+        d = dict()
+        for item in list_objs:
+            d = item.to_dictionary()
+            objs.append(dict(d))
+            d.clear()
+        with open(json_file, "w") as fh:
+            json.dump(objs, fh)
