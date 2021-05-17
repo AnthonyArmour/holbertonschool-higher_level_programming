a54db10886df3e42efa321bb05196d3903649d20
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-17 00:53:29 +0000

    added tsk 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string representation"""
-        if len(list_dictionaries) == 0 or list_dictionaries == []:
+        if not list_dictionaries or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
