76d870d7704d3e2951bfd19db1652f081b71f4c9
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-27 16:00:37 -0500

    changed count to sum task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists genres and number of shows linked to INTO 
 -- must have shows linked
-SELECT tv_genres.name AS genre, COUNT(case when tv_show_genres.show_id and tv_show_genres.genre_id is not null and tv_show_genres.genre_id = tv_genres.id then 1 else 0 end) AS number_of_shows
+SELECT tv_genres.name AS genre, SUM(case when tv_show_genres.show_id and tv_show_genres.genre_id is not null and tv_show_genres.genre_id = tv_genres.id then 1 else 0 end) AS number_of_shows
     FROM tv_genres LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     GROUP BY tv_genres.name
     ORDER BY 2 DESC;
