3eca1e700d4eab279d2f483d56afb5d12741b097
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-27 16:05:52 -0500

    last debug for task 13 added HAVING statement

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,5 @@
 SELECT tv_genres.name AS genre, SUM(case when tv_show_genres.show_id and tv_show_genres.genre_id is not null and tv_show_genres.genre_id = tv_genres.id then 1 else 0 end) AS number_of_shows
     FROM tv_genres LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     GROUP BY tv_genres.name
+    HAVING SUM(case when tv_show_genres.show_id and tv_show_genres.genre_id is not null and tv_show_genres.genre_id = tv_genres.id then 1 else 0 end) > 0
     ORDER BY 2 DESC;
