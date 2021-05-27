780b1fc6ad799e4b9bc91fd29fded7f3ecdb7df6
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-26 19:42:10 -0500

    added task 102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- displays top 3 cities temps during july and august
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(`value`) DESC LIMIT 3; 
