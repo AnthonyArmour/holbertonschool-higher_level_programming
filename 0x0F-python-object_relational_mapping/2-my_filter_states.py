eb48dda9acff5606c99cf00e76f7511629385182
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-29 19:14:32 +0000

    added task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+lists all states from selected database
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3],)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}'".format(argv[4]))
+    data = cursor.fetchall()
+    for item in data:
+        print(item)
