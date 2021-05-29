db3eec06634057853dcac71717126eeaf4d5f63c
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-29 19:37:28 +0000

    added task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all states from selected database
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3],)
+    with db.cursor() as cursor:
+        cursor.execute("""
+            SELECT 
+                * 
+            FROM 
+                states
+            WHERE
+                name = BINARY %(name)s
+            """, {
+                'name': argv[4]
+            })
+        data = cursor.fetchall()
+    for item in data:
+        print(item)
