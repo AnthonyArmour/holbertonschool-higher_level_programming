72cba2838596e7307547aeb3df29ce34f6ef10a4
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-02 21:00:13 +0000

    added task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* searches second biggest int */
+
+const mylist = process.argv;
+let mx = 0;
+if (mylist.length >= 4) {
+  let num;
+  mx = parseInt(mylist[2]);
+  for (let x = 0; x < mylist.length; x++) {
+    num = parseInt(mylist[x]);
+    if (num > mx) {
+      mx = num;
+    }
+  }
+}
+console.log(mx);
