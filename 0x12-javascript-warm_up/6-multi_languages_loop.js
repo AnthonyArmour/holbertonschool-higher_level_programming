bfd7271747ba22a2dc6292a6d6629ecdc28feab5
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-02 19:36:45 +0000

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* prints array in for loop */
+
+const myarr = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let x = 0; x < myarr.length; x++) {
+  console.log(myarr[x]);
+}
