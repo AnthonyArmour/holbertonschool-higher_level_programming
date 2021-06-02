a0f75e0cf932512002736895bf3980a67eae25cd
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-02 21:03:51 +0000

    debug task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,16 @@
 
 const mylist = process.argv;
 let mx = 0;
+let mx2 = 0;
 if (mylist.length >= 4) {
   let num;
   mx = parseInt(mylist[2]);
   for (let x = 0; x < mylist.length; x++) {
     num = parseInt(mylist[x]);
     if (num > mx) {
+      mx2 = mx;
       mx = num;
     }
   }
 }
-console.log(mx);
+console.log(mx2);
