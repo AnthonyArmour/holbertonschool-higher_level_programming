0c433e35f781d3f71066f7f7041a26204bb12a0b
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-02 21:18:16 +0000

    debug task 11 again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,11 +7,13 @@ let mx2 = 0;
 if (mylist.length >= 4) {
   let num;
   mx = parseInt(mylist[2]);
-  for (let x = 0; x < mylist.length; x++) {
+  for (let x = 3; x < mylist.length; x++) {
     num = parseInt(mylist[x]);
     if (num > mx) {
       mx2 = mx;
       mx = num;
+    } else if (num < mx && num > mx2) {
+      mx2 = num;
     }
   }
 }
